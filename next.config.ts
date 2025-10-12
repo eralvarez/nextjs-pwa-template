81b2a73f3b0d8d07d9a5d977dc863050e82bd6f3
Erick Alvarez <erick.alvarez@live.com>
2025-10-12 14:16:20 -0700

    pwav1

--- next.config.ts
+++ next.config.ts
@@ -5,6 +5,10 @@ const withPWA = require("next-pwa")({
   register: true,
   skipWaiting: true,
   disable: process.env.NODE_ENV === "development",
+  buildExcludes: [/app-build-manifest.json$/],
+  fallbacks: {
+    document: '/offline',
+  }
 });
 
 const nextConfig: NextConfig = {
