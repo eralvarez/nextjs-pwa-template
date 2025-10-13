d254f12399aff5d977e2d4f394d2e6b53907f4fa
Erick Alvarez <erick.alvarez@live.com>
2025-10-13 09:12:50 -0700

    no pinch to zoom wip

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -27,8 +27,12 @@ export default function RootLayout({
   return (
     <html lang="en">
       <head>
+        <meta
+          name="viewport"
+          content="width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=no"
+        ></meta>
         <link rel="manifest" href="/manifest.json" />
-        <meta name="theme-color" content="#0ea5e9" />
+        <meta name="theme-color" content="#ffffffff" />
         <link rel="apple-touch-icon" href="/web-app-manifest-192x192.png" />
       </head>
       <body className={`${geistSans.variable} ${geistMono.variable}`}>
